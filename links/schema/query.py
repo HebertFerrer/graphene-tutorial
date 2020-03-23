d9c6cfb6a3dfb8f57b3d093ced8ec5cefd8520eb
Hebert Ferrer <hebert.ferrer@bigbox.com.ar>
2020-03-23 14:17:16 -0300

    Filtering

--- links/schema/query.py
+++ links/schema/query.py
@@ -1,16 +1,22 @@
 import graphene
 from graphene_django import DjangoObjectType
 
+from django.db.models import Q
+
 from links.schema.types import LinkType, VoteType
 from links.models import Link, Vote
 
 
 class Query(object):
-    links = graphene.List(LinkType)
+    links = graphene.List(LinkType, search=graphene.String())
     votes = graphene.List(VoteType)
 
-    def resolve_links(self, info):
-        print("Si paso por aqui pidiendo directamente los votes")
+    def resolve_links(self, info, search=None):
+        if search:
+            return Link.objects.filter(
+                Q(url__icontains=search) |
+                Q(description__icontains=search),
+            )
         return Link.objects.all()
 
     def resolve_votes(self, info):
